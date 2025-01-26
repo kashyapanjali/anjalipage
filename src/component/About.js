b8486ab2cd54a85e0773c26f6853e251a151b2b1
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-01-26 10:41:14 +0530

    add link

--- src/component/About.js
+++ src/component/About.js
@@ -12,6 +12,8 @@ function About() {
 					<h2>Bachelor of Technology (B.Tech)</h2>
 					<p>Driems University</p>
 					<p>2022 - 2026</p>
+					<p>CGPA:9.39</p>
+					<p className="instituteLink"><a href="https://www.driems.ac.in/">College Link</a></p>
 				</div>
 			</div>
 			<div className="about-container2">
@@ -20,11 +22,16 @@ function About() {
 					<h2>Senior Secondary Education</h2>
 					<p>Sushant Public School</p>
 					<p>2019 - 2021</p>
+					<p>Marks:72%</p>
+					<p className="instituteLink"><a href ="https://www.justdial.com/Muzaffarpur/Sushant-public-school-Nunfara-Piar/9999PX621-X621-220819223206-C5Z1_BZDET">School link</a></p>
+
 				</div>
 				<div className="education-item">
 					<h2>Secondary Education</h2>
 					<p>Sushant Public School</p>
 					<p>2017 - 2019</p>
+					<p>Marks:80%</p>
+					<p className="instituteLink"><a href ="https://www.justdial.com/Muzaffarpur/Sushant-public-school-Nunfara-Piar/9999PX621-X621-220819223206-C5Z1_BZDET">School link</a></p>
 				</div>
 			</div>
 		</div>
