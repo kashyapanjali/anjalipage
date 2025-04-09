a0ad5c8df1f2517d5437a13db4f3bfa74edb3d19
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-04-10 01:26:45 +0530

    add project

--- src/component/Project.js
+++ src/component/Project.js
@@ -7,7 +7,7 @@ function Project() {
 	const projects = [
 		{
 			title: "LocationAccess App (2024)",
-			link: "https://locationwithtoken.netlify.app/",
+			link: "https://locationbuddy.netlify.app/",
 			description:
 				"A React-based web app allowing users to share real-time location embeded in tokens with full of Security. Features include WebSocket updates, CSS React JavaScript frontend, Express Node.Js for backend ,storing the data of authentication use MYSQL.",
 		},
@@ -21,18 +21,30 @@ function Project() {
 			title: "WeatherApp (2023)",
 			link: "https://anjali-weather-app.netlify.app/",
 			description:
-				"A project designed to provide users with real-time weather updates and forecasts based on their location. Built with HTML, CSS, and JavaScript, the app integrates OpenWeatherMap's API to fetch accurate weather data. Features include location-based forecast and collecting user feedback.",
+				"Developed a weather application using React.js and JavaScript, integrating the OpenWeather API for real-time forecasts. It features Google OAuth for secure authentication and AI-powered recommendations based on user location. I also implemented a real-time feedback system using Firebase Realtime Database to enhance user engagement and experience.",
+		},
+
+		{
+			title: "Employee Management System(2023)",
+			link: "https://employee-manage-app.netlify.app/",
+			description:
+				"A full-stack web application developed using React.js, Tailwind CSS, and Bootstrap on the frontend, with Node.js, Express.js, and MongoDB powering the backend. It features secure user google authentication using JWT, allowing only authorized users to access and manage data. The system supports full CRUD operations for employee records, including the ability to upload and update profile pictures.",
 		},
 	];
 
 	return (
-		<div className="work">
-			<h1 className="work-title">Work Project</h1>
-			<div className="work-container">
+		<div className='work'>
+			<h1 className='work-title'>Work Project</h1>
+			<div className='work-container'>
 				{projects.map((project, index) => (
-					<div className="work-item" key={index}>
+					<div
+						className='work-item'
+						key={index}>
 						<h2>{project.title}</h2>
-						<a href={project.link} target="_blank" rel="noopener noreferrer">
+						<a
+							href={project.link}
+							target='_blank'
+							rel='noopener noreferrer'>
 							Link
 						</a>
 						<p>{project.description}</p>
