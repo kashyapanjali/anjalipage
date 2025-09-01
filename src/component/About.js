396843c4e488536c2a12524aa46112ecba6ca463
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-09-01 13:41:59 +0530

    add link of github

--- src/component/About.js
+++ src/component/About.js
@@ -4,34 +4,43 @@ import "./About.css";
 
 function About() {
 	return (
-		<div className="about">
+		<div className='about'>
 			{/* <h1>Education</h1> */}
-			<div className="about-container1">
+			<div className='about-container1'>
 				<h1>Pursuing</h1>
-				<div className="education-item">
+				<div className='education-item'>
 					<h2>Bachelor of Technology (B.Tech)</h2>
 					<p>Driems University(Odisha)</p>
 					<p>2022 - 2026</p>
-					<p>CGPA:9.39</p>
-					<p className="instituteLink"><a href="https://www.driems.ac.in/">College Link</a></p>
+					<p>CGPA:9.17</p>
+					<p className='instituteLink'>
+						<a href='https://www.driems.ac.in/'>College Link</a>
+					</p>
 				</div>
 			</div>
-			<div className="about-container2">
+			<div className='about-container2'>
 				<h1>Highest Degree</h1>
-				<div className="education-item">
+				<div className='education-item'>
 					<h2>Senior Secondary Education</h2>
 					<p>Sushant Public School(Bihar-Muzaffarpur)</p>
 					<p>2019 - 2021</p>
 					<p>Marks:72%</p>
-					<p className="instituteLink"><a href ="https://www.justdial.com/Muzaffarpur/Sushant-public-school-Nunfara-Piar/9999PX621-X621-220819223206-C5Z1_BZDET">School link</a></p>
-
+					<p className='instituteLink'>
+						<a href='https://www.justdial.com/Muzaffarpur/Sushant-public-school-Nunfara-Piar/9999PX621-X621-220819223206-C5Z1_BZDET'>
+							School link
+						</a>
+					</p>
 				</div>
-				<div className="education-item">
+				<div className='education-item'>
 					<h2>Secondary Education</h2>
 					<p>Sushant Public School(Bihar-Muzaffarpur)</p>
 					<p>2017 - 2019</p>
-					<p>Marks:80%</p>
-					<p className="instituteLink"><a href ="https://www.justdial.com/Muzaffarpur/Sushant-public-school-Nunfara-Piar/9999PX621-X621-220819223206-C5Z1_BZDET">School link</a></p>
+					<p>Marks:79.2%</p>
+					<p className='instituteLink'>
+						<a href='https://www.justdial.com/Muzaffarpur/Sushant-public-school-Nunfara-Piar/9999PX621-X621-220819223206-C5Z1_BZDET'>
+							School link
+						</a>
+					</p>
 				</div>
 			</div>
 		</div>
--- src/component/Work.js
+++ src/component/Work.js
@@ -4,28 +4,36 @@ import "./Work.css";
 
 function Work() {
 	return (
-		<div className="work">
-			<h1 className="work-title">Recent Work Project</h1>
-			<div className="work-container">
-				<div className="work-item">
+		<div className='work'>
+			<h1 className='work-title'>Recent Work Project</h1>
+			<div className='work-container'>
+				<div className='work-item'>
 					<h2>Continue with - NeartoWomen</h2>
 					<a
-						href="https://github.com/kashyapanjali/neartowomenbackend"
-						target="_blank"
-						rel="noopener noreferrer">
+						href='https://github.com/kashyapanjali/neartowomenbackend'
+						target='_blank'
+						rel='noopener noreferrer'>
 						Link
 					</a>
+					<span>
+						<a
+							href='https://github.com/kashyapanjali/womenapp'
+							target='_blank'
+							rel='noopener noreferrer'>
+							Link
+						</a>
+					</span>
 					<p>
-						A secure and scalable <b className="bold">quick-commerce</b>{" "}
+						A secure and scalable <b className='bold'>quick-commerce</b>{" "}
 						platform exclusively for women’s health products, offering menstrual
 						care, supplements, skincare, fitness, and medical devices. Users can
 						browse, filter, and purchase products with secure checkout, order
 						tracking, and personalized recommendations. The platform supports
 						multi-vendor selling, real-time notifications, and AI-driven product
 						suggestions, ensuring a seamless shopping experience.
-						<b className="bold">
-							Tech Stack: React.js (Next.js), Node.js, Express.js, MongoDB,
-							Redux.
+						<b className='bold'>
+							Tech Stack: CSS5, React.js, Node.js, Express.js, MongoDB, Payment
+							Gateway Redux.
 						</b>
 					</p>
 				</div>
