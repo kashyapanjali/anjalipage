0fb006026b72ea4cbd8b4343e307d766a133dbc6
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2026-08-30 12:25:12 +0530

    not changed

--- src/api.js
+++ src/api.js
@@ -5,11 +5,11 @@ const API_BASE_URL = "https://anjalipagebackend.onrender.com/api/users";
 
 // Fetch portfolio data
 export const fetchPortfolio = async () => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/portfolio`);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching portfolio:", error);
-    return null;
-  }
+	try {
+		const response = await axios.get(`${API_BASE_URL}/portfolio`);
+		return response.data;
+	} catch (error) {
+		console.error("Error fetching portfolio:", error);
+		return null;
+	}
 };
